d4c8847885d431d917615b9ad3f5c704be4e83b3
ljw6632 <ljw6632@gmail.com>
2014-08-01 15:01:30 -0700

    style sheer and index.php(not completed)

---
+++ index.php
@@ -0,0 +1,66 @@
+<!--<?php
+	include("template/header.php");
+	$title = "Home";
+	$page = "pg_home";
+?>-->
+<main>
+<div class="inner_wrapper">
+	<div class="outer_div">
+		<div class="inner_div">
+			<h1>YOUR NEXT HIRE AWAITS!</h1>
+			<div class="content_01">
+
+				<img src="" alt="">
+
+				<h2>EMPLOYERS</h2>
+
+				<p>At BCIT's High-Tech Professional programs, we take pride in producing the type of graduate that
+				can quickly assimilate into the real world of information technology. This section of our website
+				outlines two major concepts: our work-term and IT projects portion of the programs as well as our
+				graduates, who are ready, willing and able to begin working for you in a myriad of different
+				capacities.</p>
+				<p>BCIT's reputation for producing job-ready graduates continues at HTP with over 150 employers
+				either hiring full-time or work-term students in addition to our graduates who have completed some
+				innovative IT projects.</p>
+			</div>
+			<div class="content_02">
+
+				<img src="" alt="">
+
+				<h2>WORK-TERMS</h2>
+
+				<p>We have work-terms in three programs: NASP, OAT and TSP. Work-terms allow students to gain
+				valuable hands-on experience onsite, in addition to alleviating your staff shortages, etc. Continuing
+				reading for details about the work-term and for the documents and links to secure a position with
+				any of the three programs.</p>
+			</div>
+			<div class="content_03">
+
+				<img src="" alt="">
+
+				<h2>PROJECT / WEBSITES</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non pellentesque ante, non
+				placerat metus. Morbi eget massa at ipsum auctor pretium. Suspendisse tincidunt tincidunt nisi, non
+				hendrerit lacus pulvinar vel. Maecenas hendrerit interdum quam sit amet interdum. Cras non iaculis
+				nulla. Sed egestas risus quis libero porttitor, sed scelerisque enim consequat. Sed consequat quam
+				sit amet dui interdum, nec rhoncus dolor consequat. Nullam consectetur blandit sem. Mauris
+				vulputate at nisi nec bibendum. Nunc elit felis, ornare vel nisl non, sodales porta elit. Fusce viverra
+				arcu eu volutpat iaculis. Mauris ornare lobortis nulla, ac adipiscing augue.</p>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#"><span>NASP</span><br><hr><span>Network<br>Administration and<br>Security<br>Professional</span><hr></a></li>
+					<li><a href="#"><span>OAT</span><br><hr><span>Office<br>Administration with<br>Technology</span><hr></a></li>
+					<li><a href="#"><span>SSD</span><br><hr><span>Software<br>Systems<br>Developer</span><hr></a></li>
+					<li><a href="#"><span>TWD</span><br><hr><span>Technical<br>Web<br>Designer</span><hr></a></li>
+					<li><a href="#"><span>TSP</span><br><hr><span>Technology<br>Support<br>Professional</span><hr></a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</div>
+</main>
+<!--<?php
+	include("template/footer.php");
+?>-->
